a36513dd8faf89bd25c48eb89a04aba5ecde1e0f
justNeto <continental.lelgg.2b@gmail.com>
2025-02-09 00:42:49 -0600

    Reverted back to changes in nix

--- flake.nix
+++ flake.nix
@@ -25,36 +25,15 @@
         ghostty = {
             url = "github:ghostty-org/ghostty";
         };
-
-        hyprland.url = "github:hyprwm/Hyprland?submodules=1";
-
-        hy3 = {
-            url = "github:outfoxxed/hy3";
-            inputs.hyprland.follows = "hyprland";
-        };
     };
 
-    outputs = { self, home-manager, ghostty, hyprland, ... }@inputs:
+    outputs = { self, home-manager, ghostty, ... }@inputs:
         let
             system 	= "x86_64-linux";
             unstable = import inputs.nixpkgs-unstable {inherit system; };
             pkgs = inputs.nixpkgs;
         in
         {
-            homeConfigurations."neto@justNeto-nixos" = home-manager.lib.homeManagerConfiguration {
-                modules = [
-                    hyprland.homeManagerModules.default
-
-                    {
-                        wayland.windowManager.hyprland = {
-                            enable = true;
-                            package = inputs.hyprland.packages.${system}.hyprland;
-                            portalPackage = inputs.hyprland.packages.${system}.xdg-desktop-portal-hyprland;
-                            plugins = [ inputs.hy3.${system}.hy3 ];
-                        };
-                    }
-                ];
-            };
             nixosConfigurations.justNeto-nixos = pkgs.lib.nixosSystem
             {
                 specialArgs = { inherit inputs; };
